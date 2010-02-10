16f5f76ede60d62adbe16571eb0ee81e49bd3ce6
Stefan Behnel <scoder@users.berlios.de>
2010-02-10 10:48:22 +0100

    allow selecting specific engines in benchmark

--- bench.py
+++ bench.py
@@ -2,6 +2,9 @@
 Benchmark comparison between acora search and re.findall()
 """
 
+COMPARED_IMPLEMENTATIONS = ["pa", "ca", "re"]
+REPEAT_COUNT = 5
+
 import re
 import sys
 import timeit
@@ -43,45 +46,61 @@ def prepare_benchmark_data():
     return search_string, all_keywords
 
 def compare_search(s, filename, ignore_case, *keywords):
-    t = time()
-    builder = AcoraBuilder(keywords)
-    py_acora = builder.build(ignore_case=ignore_case, acora=PyAcora)
-    setup_pya = time() - t
-    t = time()
-    t = time()
-    builder = AcoraBuilder(keywords)
-    c_acora  = builder.build(ignore_case=ignore_case)
-    setup_ca = time() - t
-    if hasattr(keywords[0], 'encode'): # unicode in Py3?
-        kw_regexp = '|'.join(keywords)
-    else:
-        kw_regexp = '|'.encode('ASCII').join(keywords)
-    if ignore_case:
-        regexp = re.compile(kw_regexp, re.I)
-    else:
-        regexp = re.compile(kw_regexp)
-    setup_re = time() - t
+    setup_pya = setup_cya = setup_re = 0
+    run_pa = 'pa' in COMPARED_IMPLEMENTATIONS
+    run_ca = 'ca' in COMPARED_IMPLEMENTATIONS
+    run_re = 're' in COMPARED_IMPLEMENTATIONS
+
+    if run_pa:
+        t = time()
+        builder = AcoraBuilder(keywords)
+        py_acora = builder.build(ignore_case=ignore_case, acora=PyAcora)
+        setup_pya = time() - t
+        t = time()
+    if run_ca:
+        t = time()
+        builder = AcoraBuilder(keywords)
+        c_acora  = builder.build(ignore_case=ignore_case)
+        setup_ca = time() - t
+    if run_re:
+        if hasattr(keywords[0], 'encode'): # unicode in Py3?
+            kw_regexp = '|'.join(keywords)
+        else:
+            kw_regexp = '|'.encode('ASCII').join(keywords)
+        if ignore_case:
+            regexp = re.compile(kw_regexp, re.I)
+        else:
+            regexp = re.compile(kw_regexp)
+        setup_re = time() - t
     print("Case %ssensitive %s - setup times: PA: %.4f, CA: %.4f, RE: %.4f" % (
             ignore_case and 'in' or '',
             builder.for_unicode and 'unicode' or 'bytes',
             setup_pya, setup_ca, setup_re))
 
-    timings = timeit.Timer(partial(py_acora.findall, s)).repeat(number=10)
-    print("TIME(paS): %.3f" % min(timings))
-    timings = timeit.Timer(partial(c_acora.findall, s)).repeat(number=10)
-    print("TIME(caS): %.3f" % min(timings))
+    if run_pa:
+        timings = timeit.Timer(partial(py_acora.findall, s)).repeat(number=REPEAT_COUNT)
+        print("TIME(paS): %.3f" % min(timings))
+    if run_ca:
+        timings = timeit.Timer(partial(c_acora.findall, s)).repeat(number=REPEAT_COUNT)
+        print("TIME(caS): %.3f" % min(timings))
     if filename:
-        timings = timeit.Timer(partial(py_acora.filefindall, filename)).repeat(number=10)
-        print("TIME(paF): %.3f" % min(timings))
-        timings = timeit.Timer(partial(c_acora.filefindall, filename)).repeat(number=10)
-        print("TIME(caF): %.3f" % min(timings))
-    timings = timeit.Timer(partial(regexp.findall, s)).repeat(number=10)
-    print("TIME(reS): %.3f" % min(timings))
+        if run_pa:
+            timings = timeit.Timer(partial(py_acora.filefindall, filename)).repeat(number=REPEAT_COUNT)
+            print("TIME(paF): %.3f" % min(timings))
+        if run_ca:
+            timings = timeit.Timer(partial(c_acora.filefindall, filename)).repeat(number=REPEAT_COUNT)
+            print("TIME(caF): %.3f" % min(timings))
+    if run_re:
+        timings = timeit.Timer(partial(regexp.findall, s)).repeat(number=REPEAT_COUNT)
+        print("TIME(reS): %.3f" % min(timings))
 
-    return (c_acora.findall(s), py_acora.findall(s),
-            filename and c_acora.filefindall(filename) or [],
-            filename and py_acora.filefindall(filename) or [],
-            regexp.findall(s))
+    return (
+        run_pa and py_acora.findall(s) or None,
+        run_ca and c_acora.findall(s) or None,
+        run_pa and (filename and py_acora.filefindall(filename)) or None,
+        run_ca and (filename and c_acora.filefindall(filename)) or None,
+        run_re and regexp.findall(s) or None
+        )
 
 def run_benchmark(search_string, all_keywords):
     search_string_lower = search_string.lower()
@@ -101,35 +120,33 @@ def run_benchmark(search_string, all_keywords):
             keywords_lower = [ kw.lower() for kw in keywords ]
 
             results = compare_search(search_string, None, False, *keywords)
-            assert_equal(results, results[0], search_string, keywords)
-            assert_equal(results, results[1], search_string, keywords)
+            for result in results[:2]:
+                assert_equal(results, result, search_string, keywords)
 
             results = compare_search(search_string, None, True, *keywords)
-            assert_equal(results, results[0], search_string_lower, keywords_lower)
-            assert_equal(results, results[1], search_string_lower, keywords_lower)
+            for result in results[:2]:
+                assert_equal(results, result, search_string_lower, keywords_lower)
 
             keywords = [ keyword.encode('ASCII') for keyword in keywords ]
 
             results = compare_search(bytes_search_string, filename, False, *keywords)
-            assert_equal(results, results[0], bytes_search_string, keywords)
-            assert_equal(results, results[1], bytes_search_string, keywords)
-            assert_equal(results, results[2], bytes_search_string, keywords)
-            assert_equal(results, results[3], bytes_search_string, keywords)
+            for result in results[:4]:
+                assert_equal(results, result, bytes_search_string, keywords)
 
             if sys.version_info[0] < 3:
                 keywords_lower = [ keyword.encode('ASCII') for keyword in keywords_lower ]
                 # case-insensitive search in byte strings is not supported in Py3
                 results = compare_search(bytes_search_string, filename, True, *keywords)
-                assert_equal(results, results[0], bytes_search_string_lower, keywords_lower)
-                assert_equal(results, results[1], bytes_search_string_lower, keywords_lower)
-                assert_equal(results, results[2], bytes_search_string_lower, keywords_lower)
-                assert_equal(results, results[3], bytes_search_string_lower, keywords_lower)
+                for result in results[:4]:
+                    assert_equal(results, result, bytes_search_string_lower, keywords_lower)
 
 def assert_equal(results, result, search_string, keywords):
+    if result is None:
+        return
     assert len(result) == sum(map(search_string.count, keywords)), \
         "EXPECTED: %d, got %s" % (
         sum(map(search_string.count, keywords)),
-        list(map(len, results)))
+        [ (len(res) if res is not None else None) for res in results] )
 
 if __name__ == '__main__':
     run_benchmark(*prepare_benchmark_data())
