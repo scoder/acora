1ae8a87d53b6c04ce11ecab74b82431bf25c379d
Stefan Behnel <stefan_ml@behnel.de>
2023-09-12 17:20:48 +0200

    Remove the need for an external declarations .h file by moving the declarations into the _cacora.pyx.

--- acora/_cacora.pyx
+++ acora/_cacora.pyx
@@ -24,7 +24,22 @@ from ._acora cimport (
 cdef extern from * nogil:
     ssize_t read(int fd, void *buf, size_t count)
 
-cdef extern from "acora_defs.h":
+cdef extern from *:
+    """
+    #if PY_VERSION_HEX >= 0x030C0000 && !defined(PyUnicode_IS_READY)
+      #define PyUnicode_IS_READY(op)    (1)
+      #define PyUnicode_AS_UNICODE(u)   (NULL)
+      #define PyUnicode_GET_SIZE(u)     (0)
+
+    #elif PY_VERSION_HEX <= 0x03030000 && !(defined(CYTHON_PEP393_ENABLED) && CYTHON_PEP393_ENABLED)
+      #define PyUnicode_IS_READY(op)    (0)
+      #define PyUnicode_GET_LENGTH(u)   PyUnicode_GET_SIZE(u)
+      #define PyUnicode_KIND(u)         (sizeof(Py_UNICODE))
+      #define PyUnicode_DATA(u)         ((void*)PyUnicode_AS_UNICODE(u))
+      #define PyUnicode_READ(kind, data, index)   \
+            (((void)kind), (Py_UCS4) ((Py_UNICODE*)data)[index])
+    #endif
+    """
     # PEP 393
     cdef bint PyUnicode_IS_READY(object u)
     cdef Py_ssize_t PyUnicode_GET_LENGTH(object u)
